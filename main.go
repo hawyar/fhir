43ebc7572b29edd806b9aed8021f8252603aa533
hawyar <hawyarxoshnaw@gmail.com>
2021-08-24 04:47:03 -0400

    deploy it

--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/samply/golang-fhir-models/fhir-models/fhir"
 	model "github.com/samply/golang-fhir-models/fhir-models/fhir"
 )
-
 
 var patients = make([]model.Patient, 20)
 
@@ -17,18 +18,48 @@ func PatientHandler(w http.ResponseWriter, r *http.Request) {
 	id := "12"
 	given := "Shane"
 	family := "Early"
+	var NameUse fhir.NameUse = 0
+	nameEnd := "2012"
+	phone := "1234567890"
+	current := time.Now().String()
+	active := true
 
 	for i := 0; i < 20; i++ {
 		patients[i] = model.Patient{
 			Id: &id,
-			Name: []model.HumanName{
-				{
-					Family: &given,
-					Given:  []string{family},
+			Meta: &model.Meta{
+				LastUpdated: &current,
+				VersionId: &id,
+				Extension: []model.Extension{
+					{
+						Url: "http://hl7.org/fhir/StructureDefinition/us-core-patient-birthDate",
+						Id:  &id,
+						Extension: []model.Extension{
+							{
+								Url: "http://hl7.org/fhir/StructureDefinition/us-core-birthDate",
+							},
+						},
+					},
 				},
 			},
+			Name: []model.HumanName{
+				{
+					Use: &NameUse,
+					Family: &given,
+					Given:  []string{family},
+					Period: &model.Period{
+						End:  &nameEnd,
+					},
+				},
+			},
+			Telecom: []model.ContactPoint{
+				{
+					Id: &id,
+					Value: &phone,
+				},
+			},
+			Active: &active,
 		}
-
 		vars := mux.Vars(r)
 
 		id = vars["id"]
@@ -61,6 +92,7 @@ func main() {
 
 	r := mux.NewRouter()
 
+	
 	r.HandleFunc("/", Index)
 	r.HandleFunc("/patient/{id}", PatientHandler)
 	r.HandleFunc("/patient", PatientsIndexHandler)
