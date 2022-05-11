d36697a2bc9b21e2473003f612e2e5a013e93312
hawyar <hawyarfa@gmail.com>
2022-05-10 21:03:47 -0400

    clean middleware

--- middleware.go
+++ middleware.go
@@ -8,7 +8,11 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-// Essential middleware for FHIR server
+type RequestCtx struct {
+	Format string `json:"format"` // The format to return the resource in, could be either json or xml
+	Prefer string `json:"prefer"` // The format to return the resource in, could be either minimal, representation, or OperationOutcome
+}
+
 func (s *Server) MountMiddlewares() {
 	r := s.Router
 	r.Use(middleware.RequestID)
@@ -17,68 +21,42 @@ func (s *Server) MountMiddlewares() {
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Timeout(60 * time.Second))
 	r.Use(middleware.AllowContentType("application/fhir+json", "application/fhir+xml"))
+	r.Use(middleware.StripSlashes)
+	r.Use(PreferRequestCtx)
 	r.Use(SetDefaultTimeZone)
-	r.Use(ResourceContentType)
 }
 
-// ResourceContentType sets the requested resource content type
-func ResourceContentType(next http.Handler) http.Handler {
+func PreferRequestCtx(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-		param := r.URL.Query().Get("_format")
-
-		format := "json"
-
-		if param == "xml" {
-			format = "xml"
-			w.Header().Set("Content-Type", "application/fhir+xml")
-		} else {
-			w.Header().Set("Content-Type", "application/fhir+json")
+		reqCtx := RequestCtx{
+			Format: "json",
+			Prefer: "representation",
 		}
 
-		ctx = context.WithValue(ctx, "format", format)
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
-}
+		contentType := r.URL.Query().Get("_format")
 
-// PreferTransactionCtx specifies what the server return for a transaction request. Could be one of minimal, representative. See
-func PreferTransactionCtx(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if contentType == "xml" {
+			reqCtx.Format = "xml"
+		}
 
-		// if not a transaction request (POST, PUT, PATCH) then skip
+		// only check if its transaction request
 		if r.Method != "POST" && r.Method != "PUT" && r.Method != "PATCH" {
-			next.ServeHTTP(w, r)
-			return
+			// check for prefer header
+			prefer := r.Header.Get("Prefer")
+
+			if prefer == "return=minimal" {
+				reqCtx.Prefer = "minimal"
+			}
+
+			if prefer == "return=OperationOutcome" {
+				reqCtx.Prefer = "OperationOutcome"
+			}
 		}
 
-		ctx := r.Context()
+		ctx := context.WithValue(r.Context(), RequestCtx{}, reqCtx)
+		next.ServeHTTP(w, r.WithContext(ctx))
 
-		prefer := r.Header.Get("Prefer")
-
-		// if not set then skip
-		if prefer == "" {
-			next.ServeHTTP(w, r)
-			return
-		}
-
-		if prefer == "return=minimal" {
-			w.WriteHeader(http.StatusNoContent)
-			return
-		}
-
-		if prefer == "return=representation" {
-			ctx := context.WithValue(ctx, "PreferTxReturn", "representation")
-			next.ServeHTTP(w, r.WithContext(ctx))
-		}
-
-		if prefer == "return=OperationOutcome" {
-			ctx := context.WithValue(ctx, "PreferTxReturn", "OperationOutcome")
-			next.ServeHTTP(w, r.WithContext(ctx))
-		}
-
-		next.ServeHTTP(w, r)
 	})
-
 }
 
 func SetDefaultTimeZone(next http.Handler) http.Handler {
